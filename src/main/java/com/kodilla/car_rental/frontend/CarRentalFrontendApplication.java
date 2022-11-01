aab5e59f9d73721acea06ba5d0729ad440f7935a
Jakub Mihuka <jakubmihulka02@gmail.com>
2022-11-01 18:47:52 +0100

    First commit

---
+++ src/main/java/com/kodilla/car_rental/frontend/CarRentalFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.car_rental.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalFrontendApplication.class, args);
+    }
+
+}
